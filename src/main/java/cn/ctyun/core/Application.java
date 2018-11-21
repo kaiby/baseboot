23f656ade586355933dbacff65278133359efd93
kaiby <kaiby99@126.com>
2018-11-21 15:59:40 +0800

    优化Druid连接池配置； 优化分页插件

--- src/main/java/cn/ctyun/core/Application.java
+++ src/main/java/cn/ctyun/core/Application.java
@@ -3,13 +3,9 @@ package cn.ctyun.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 @EnableAsync
-@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
